09e3bba5221d48fc520d4af7ae1189fecd07f04d
jottachaves <joaocarlos0041@gmail.com>
2024-05-17 21:06:17 +0000

    dio-java-basico

--- meu projeto java/src/App.java
+++ meu projeto java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Opa,iai pessoal!");
+        System.out.println("Opa,,,,iai pessoal!");
     }
 }
